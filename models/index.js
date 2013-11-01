4fa757dd04c71b017fbd5132a9abe5bafa7c0494
Petchesi Gabriel Horatiu <pghoratiu@gmail.com>
2013-11-01 17:36:48 +0200

    Add missing files.

---
+++ models/index.js
@@ -0,0 +1,30 @@
+var Sequelize = require('sequelize');
+var config    = require('config').database;  // we use node-config to handle environments
+
+// initialize database connection
+var sequelize = new Sequelize(
+  config.name,
+  config.username,
+  config.password,
+  config.options
+);
+
+// load models
+var models = [
+  'Article',
+  'User'
+];
+models.forEach(function(model) {
+  module.exports[model] = sequelize.import(__dirname + '/' + model);
+});
+
+// describe relationships
+(function(m) {
+  m.PhoneNumber.belongsTo(m.User);
+  m.Task.belongsTo(m.User);
+  m.User.hasMany(m.Task);
+  m.User.hasMany(m.PhoneNumber);
+})(module.exports);
+
+// export connection
+module.exports.sequelize = sequelize;
